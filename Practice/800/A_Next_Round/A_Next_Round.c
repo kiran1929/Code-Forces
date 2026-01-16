9d03654927d97db33c89b755696ac03b569b4671
kirandeep gudepu <gudepukirandeep@gmail.com>
2026-01-16 22:51:16 +0530

    Add A_Next_Round.c for counting qualifying scores

---
+++ Practice/800/A_Next_Round/A_Next_Round.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,k,count=0;
+    scanf("%d %d",&n,&k);
+    int a[n];
+    for (int i=0;i<n;++i)
+    {
+        scanf("%d",&a[i]);
+    }
+    for (int i=0;i<n;++i)
+    {
+        if (a[i] >= a[k-1] && a[i] > 0)
+        {
+            ++count;
+        }
+    }
+    printf("%d",count);
+
+    return 0;
+}

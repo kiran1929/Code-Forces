177541fa14561d8d1473452d91c55385ef8224da
kirandeep gudepu <gudepukirandeep@gmail.com>
2026-09-23 21:41:15 +0530

    Add Eating Game [A] from Codeforces

---
+++ 2200/A - Eating Game/solution.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        int n;
+        cin >> n;
+ 
+        vector<int> a(n);
+ 
+        int mx = 0;
+ 
+        for(int i = 0; i < n; i++) 
+        {
+            cin >> a[i];
+            if(a[i] > mx) 
+            {
+                mx = a[i];
+            }
+        }
+ 
+        int count = 0;
+ 
+        for(int i = 0; i < n; i++) 
+        {
+            if(a[i] == mx) 
+            {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+ 
+    return 0;
+}

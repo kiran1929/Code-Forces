4a0522c28621d10dc23c1fb60944efeb5a4f2a27
kirandeep gudepu <gudepukirandeep@gmail.com>
2026-09-23 21:40:57 +0530

    Add Way Too Long Words [A] from Codeforces

---
+++ 71/A - Way Too Long Words/solution.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main()
+ 
+{
+    int n;
+    scanf("%d",&n);
+    while(n--)
+    {
+        
+        char str[200000];
+        scanf("%s",&str);
+        int length = strlen(str);
+        if (length > 10)
+        {
+            printf("%c",str[0]);
+            printf("%d",length-2);
+            printf("%c
+",str[length-1]);
+            
+        }
+        else
+        {
+            printf("%s
+", str);
+        }
+    }
+    //printf("%s",str);
+    return 0;
+}

729c65bb9582ffe262a74a34ec81d97fb3dc1503
kirandeep gudepu <gudepukirandeep@gmail.com>
2026-09-23 21:41:19 +0530

    Add Watermelon [A] from Codeforces

---
+++ 4/A - Watermelon/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ 
+ 
+int main()
+{
+    int n;
+    cin >> n;
+    
+    if (n == 2)
+    {
+        
+        cout << "NO";
+        return 0;
+    }
+    
+    if (n % 2 == 0)
+        cout << "YES";
+    else 
+        cout << "NO";
+ 
+    return 0;
+}

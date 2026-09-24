bd6ba5200106ecc0afc8d87c30bcbbc67137230b
kirandeep gudepu <gudepukirandeep@gmail.com>
2026-09-24 11:49:47 +0530

    Add Doremy's Paint 3 [A] from Codeforces

---
+++ 1890/A - Doremy's Paint 3/solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--){
+        long long n;
+        cin >> n;
+        
+        vector<long long> a(n);
+        for(int i = 0; i < n; i++)
+            cin >> a[i];
+            
+        map<long long, long long> freq;
+        for(int i = 0; i < n; i++)
+            freq[a[i]]++;
+            
+        if(freq.size() >= 3)
+            cout << "NO" << endl;
+            
+        else
+        {
+            long long freq1 = freq.begin()->second;
+            long long freq2 = freq.rbegin()->second;
+            
+            if(freq1 == freq2)
+                cout << "YES" << endl;
+            else if (n % 2 == 1 && abs(freq1 - freq2) == 1)
+                cout << "Yes" << endl;
+            else
+                cout << "NO" << endl;
+        }
+    }
+ 
+    return 0;
+}

083bbd60b3e81f75b482da68b6d19e4a39799a9b
kirandeep gudepu <gudepukirandeep@gmail.com>
2026-09-26 22:42:23 +0530

    Add Desorting [A] from Codeforces

---
+++ 1853/A - Desorting/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n;
+        cin >> n;
+        
+        vector<int> v(n);
+        
+        for(int i = 0; i < n; i++)
+            cin >> v[i];
+            
+        int ans = INT_MAX;
+        for(int i = 1; i < n; i++)
+            ans = min(ans, max(0, (v[i] - v[i - 1] + 2) / 2));
+            
+        cout << ans << endl;
+            
+            
+    }
+ 
+    return 0;
+}
